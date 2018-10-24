2cdfd1291be987f5a4558789097f294fcaf330bf
Alexandre Moreau <alexandre@deepomatic.com>
2018-10-24 16:00:45 +0200

    refactor args parsing

--- deepoctl/cli_parser.py
+++ deepoctl/cli_parser.py
@@ -10,29 +10,22 @@ def parse_args(args):
     subparsers.required = True
 
     infer_parser = subparsers.add_parser('infer', help="Run inference on a file or directory and outputs a JSON file with the recognition results.")
-    infer_parser.add_argument('-i', '--input', help="Path on which inference should be run. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory. If the given path is a directory, it will recursively run inference on all the supported files in this directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-o', '--output', help="Path in which output should be written. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-r', '--recognition_id', help="Recognition version ID")
-    infer_parser.add_argument('-u', '--amqp_url', help="AMQP url")
-    infer_parser.add_argument('-k', '--routing_key', help="Recognition routing key")
     infer_parser.set_defaults(func=infer)
 
-    infer_parser = subparsers.add_parser('draw', help="Generate new images and videos with inference results drawn on them. Runs inference if JSON has not yet been generated.")
-    infer_parser.add_argument('-i', '--input', help="Path on which inference should be run. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory. If the given path is a directory, it will recursively run inference on all the supported files in this directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-o', '--output', help="Path in which output should be written. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-r', '--recognition_id', help="Recognition version ID")
-    infer_parser.add_argument('-u', '--amqp_url', help="AMQP url")
-    infer_parser.add_argument('-k', '--routing_key', help="Recognition routing key")
-    infer_parser.set_defaults(func=draw)
+    draw_parser = subparsers.add_parser('draw', help="Generate new images and videos with inference results drawn on them. Runs inference if JSON has not yet been generated.")
+    draw_parser.set_defaults(func=draw)
 
-    infer_parser = subparsers.add_parser('blur', help="Generate new images and videos with inference results drawn on them. Runs inference if JSON has not yet been generated.")
-    infer_parser.add_argument('-i', '--input', help="Path on which inference should be run. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory. If the given path is a directory, it will recursively run inference on all the supported files in this directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-o', '--output', help="Path in which output should be written. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
-    infer_parser.add_argument('-r', '--recognition_id', help="Recognition version ID")
-    infer_parser.add_argument('-u', '--amqp_url', help="AMQP url")
-    infer_parser.add_argument('-k', '--routing_key', help="Recognition routing key")
-    infer_parser.set_defaults(func=blur)
+    blur_parser = subparsers.add_parser('blur', help="Generate new images and videos with inference results blurred out. Runs inference if JSON has not yet been generated.")
+    blur_parser.set_defaults(func=blur)
 
+    parsers = [infer_parser, draw_parser, blur_parser]
+    for parser in parsers:
+        parser.add_argument('-i', '--input', help="Path on which inference should be run. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory. If the given path is a directory, it will recursively run inference on all the supported files in this directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
+        parser.add_argument('-o', '--output', help="Path in which output should be written. It can be an image (supported formats: *{}), a video (supported formats: *{}) or a directory.".format(', *'.join(ImageInputData.supported_formats), ', *'.join(VideoInputData.supported_formats)))
+        parser.add_argument('-r', '--recognition_id', help="Recognition version ID")
+        parser.add_argument('-u', '--amqp_url', help="AMQP url")
+        parser.add_argument('-k', '--routing_key', help="Recognition routing key")
+        parser.add_argument('-t', '--threshold', help="Threshold under which inference result should be ignored")
     return argparser.parse_args(args)
 
 def run(args):
