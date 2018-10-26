8135d97d50f117eeb6c1fdc9b2fbb2a362c5b330
Alexis Jacob <alexis.jacob@epitech.eu>
2018-10-26 11:43:11 +0200

    first version

--- deepoctl/cli_parser.py
+++ deepoctl/cli_parser.py
@@ -1,6 +1,7 @@
 import argparse
 from deepoctl.cmds.infer import main as infer
 from deepoctl.cmds.draw import main as draw
+from deepoctl.cmds.feedback import main as feedback
 from deepoctl.input_data import supported_image_formats, supported_video_formats
 
 def parse_args(args):
@@ -18,6 +19,12 @@ def parse_args(args):
     infer_parser.add_argument('-r', '--recognition_id', help="Recognition version ID")
     infer_parser.set_defaults(func=draw)
 
+    feedback_parser = subparsers.add_parser('feedback', help='Send images from a folder to deepomatic studio')
+    feedback_parser.add_argument('dataset_name', type=str, help='the name of the dataset')
+    feedback_parser.add_argument('org_slug', type=str, help='the slug of your organization')
+    feedback_parser.add_argument('path', type=str, nargs='+', help='path to a file or a folder')
+    feedback_parser.add_argument('-r', '--recursive', dest='recursive', action='store_true', help='all files in subdirectory')
+    feedback_parser.set_defaults(func=feedback, recursive=False)
     return argparser.parse_args(args)
 
 def run(args):
