1b7281e4c95b0527d9c19c53ddc51bb267f4f3a9
Leo Paillier <leo.paillier@gmail.com>
2019-02-21 13:35:19 +0100

    Review feedback fixes
    - Adds newline at end of file
    - Removes projet_urls key

--- deepomatic/cli/version.py
+++ deepomatic/cli/version.py
@@ -5,9 +5,3 @@ __author__ = 'deepomatic'
 __author_email__ = 'support@deepomatic.com'
 __url__ = 'https://github.com/Deepomatic/deepocli'
 __license__ = 'MIT License'
-__project_urls__ = {
-    'Product': 'https://www.deepomatic.com',
-    'Documentation': 'https://developers.deepomatic.com/docs/v0.7',
-    'Source': 'https://github.com/Deepomatic/deepocli',
-    'Tracker': 'https://github.com/Deepomatic/deepocli/issues',
-}
