ab235656895cfbc552cc5ac015fe6193c9a0a654
Leo Paillier <leo.paillier@gmail.com>
2019-02-21 09:10:25 +0100

    Prepare deepomatic.cli for Pypi releases
    - Changes namespace from deepocli to deepomati.cli
    - Adds MIT License file
    - Adds Makefile
    - Updates documentation
    - Follows proper module implementation from deepomatic.api
    - Adds setup.cfg file
    - Matches setup.py to deepomatic.api

---
+++ deepomatic/cli/version.py
@@ -0,0 +1,13 @@
+__title__ = 'deepomatic-cli'
+__description__ = 'Deepomatic CLI'
+__version__ = '0.2.2'
+__author__ = 'deepomatic'
+__author_email__ = 'support@deepomatic.com'
+__url__ = 'https://github.com/Deepomatic/deepocli'
+__license__ = 'MIT License'
+__project_urls__ = {
+    'Product': 'https://www.deepomatic.com',
+    'Documentation': 'https://developers.deepomatic.com/docs/v0.7',
+    'Source': 'https://github.com/Deepomatic/deepocli',
+    'Tracker': 'https://github.com/Deepomatic/deepocli/issues',
+}
