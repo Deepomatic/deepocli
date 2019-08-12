013495501a867370cebc88fd2744b6effffcc551
Alexandre Moreau <alexandre@deepomatic.com>
2019-08-12 15:53:44 +0200

    decorator that prevents class instanciation when deepomatic rpc is not available

--- deepomatic/cli/output_data.py
+++ deepomatic/cli/output_data.py
@@ -5,6 +5,7 @@ import cv2
 import logging
 import traceback
 from .thread_base import Thread
+from .workflow import requires_deepomatic_rpc
 from .common import Empty, write_frame_to_disk, SUPPORTED_IMAGE_OUTPUT_FORMAT, SUPPORTED_VIDEO_OUTPUT_FORMAT
 from .cmds.studio_helpers.vulcan2studio import transform_json_from_vulcan_to_studio
 from .exceptions import DeepoUnknownOutputError, DeepoSaveJsonToFileError
@@ -13,6 +14,10 @@ from .exceptions import DeepoUnknownOutputError, DeepoSaveJsonToFileError
 LOGGER = logging.getLogger(__name__)
 DEFAULT_OUTPUT_FPS = 25
 
+try:
+    from deepomatic.rpc.client import Client
+except:
+    pass
 
 try:
     # https://stackoverflow.com/questions/908331/how-to-write-binary-data-to-stdout-in-python-3
