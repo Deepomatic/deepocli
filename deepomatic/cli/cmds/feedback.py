a9d90fcf37fc91e97c38fc1a19b731b3ace4de16
Alexis Jacob <alexis@deepomatic.com>
2019-11-07 18:19:35 +0100

    dependencies

--- deepomatic/cli/cmds/feedback.py
+++ deepomatic/cli/cmds/feedback.py
@@ -26,86 +26,14 @@ API_HOST = os.getenv('STUDIO_URL', 'https://studio.deepomatic.com/api/')
 
 ###############################################################################
 
-class StudioHTTPHelper(HTTPHelper):
-    def __init__(self, app_id=None, api_key=None, verify_ssl=None,
-                 host=None, check_query_parameters=True,
-                 user_agent_prefix='', pool_maxsize=20,
-                 requests_timeout=RequestsTimeout.FAST, **kwargs):
-        
-        self.http_retry = kwargs.pop('http_retry', HTTPRetry())
-
-        if len(kwargs) > 0:
-            raise TypeError("Too many parameters. HTTPRetry does not handle kwargs: {}".format(kwargs))
-
-        self.requests_timeout = requests_timeout
-
-        if host is None:
-            host = os.getenv('DEEPOMATIC_API_URL', API_HOST)
-        if verify_ssl is None:
-            verify = os.getenv('DEEPOMATIC_API_VERIFY_TLS', '1') == '1'
-        if app_id is None:
-            app_id = os.getenv('DEEPOMATIC_APP_ID')
-        if api_key is None:
-            api_key = os.getenv('DEEPOMATIC_API_KEY')
-        if app_id is None or api_key is None:
-            raise DeepomaticException("Please specify 'app_id' and 'api_key' either by passing those values to the client"
-                                      " or by defining the DEEPOMATIC_APP_ID and DEEPOMATIC_API_KEY environment variables.")
-
-        if not host.endswith('/'):
-            host += '/'
-
-        python_version = "{0}.{1}.{2}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
-
-        user_agent_params = {
-            'package_title': __title__,
-            'package_version': __version__,
-            'requests_version': requests.__version__,
-            'python_version': python_version,
-            'platform': platform.platform()
-        }
-
-        user_agent_list = []
-
-        if user_agent_prefix:
-            user_agent_list.append(user_agent_prefix)
-
-        user_agent_list += [
-            '{package_title}-python-client/{package_version}',
-            'requests/{requests_version}',
-            'python/{python_version} platform/{platform}',
-        ]
-
-        self.user_agent = ' '.join(user_agent_list).format(**user_agent_params)
-
-        self.api_key = str(api_key)
-        self.app_id = str(app_id)
-        self.verify = verify
-        self.host = host
-        self.resource_prefix = host
-
-        # This is only used in mixins, this should not stay here
-        self.check_query_parameters = check_query_parameters
-
-        headers = {
-            'User-Agent': self.user_agent,
-            'X-APP-ID': self.app_id,
-            'X-API-KEY': self.api_key,
-        }
-        self.session = requests.Session()
-        self.session.headers.update(headers)
-        # Use pool_maxsize to cache connections for the same host
-        adapter = requests.adapters.HTTPAdapter(pool_maxsize=pool_maxsize)
-        self.session.mount('http://', adapter)
-        self.session.mount('https://', adapter)
-
 
 class Client(object):
     def __init__(self, token=None, verify_ssl=True, check_query_parameters=True, host=None, user_agent_prefix='', pool_maxsize=GREENLET_NUMBER):
         if host is None:
             host = API_HOST
 
-        self.http_helper = StudioHTTPHelper(api_key=token, verify_ssl=None, host=host, check_query_parameters=check_query_parameters,
-                                      user_agent_prefix=user_agent_prefix, pool_maxsize=pool_maxsize)
+        self.http_helper = HTTPHelper(api_key=token, verify_ssl=None, host=host, check_query_parameters=check_query_parameters,
+                                      user_agent_prefix=user_agent_prefix, pool_maxsize=pool_maxsize, version=None)
         self.task = Task(self.http_helper)
 
 
