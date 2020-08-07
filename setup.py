3c6fbd9e2af6d854909b5a6589b9ee1acf859910
Iacopo Breschi <iacopo.breschi@deepomatic.com>
2020-08-07 10:16:00 +0200

    Update setup.py
    Co-authored-by: Thomas Riccardi <thomas@deepomatic.com>

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 # Read requirements
 install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'), session='hack')
-# for pip 20.2.1
 try:
-    requirements = [str(ir.req) for ir in install_reqs]
-except Exception:
     requirements = [str(ir.requirement) for ir in install_reqs]
+    
+except Exception:  # for pip < 20.1
+    requirements = [str(ir.req) for ir in install_reqs]
 
 namespaces = ['deepomatic']
 
