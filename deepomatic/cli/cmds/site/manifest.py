78e4aaf768f0515f94fb11eafac31b734d654869
Hugo Maingonnat <hugo@deepomatic.com>
2020-09-29 20:58:05 +0200

    fix help

--- deepomatic/cli/cmds/site/manifest.py
+++ deepomatic/cli/cmds/site/manifest.py
@@ -10,7 +10,7 @@ class ManifestCommand(Command):
     def setup(self, subparsers):
         parser = super(ManifestCommand, self).setup(subparsers)
         parser.add_argument('-i', '--id', required=True, type=str, help="Site id")
-        parser.add_argument('-t', '--target', required=True, type=str, help="Kubernetes target")
+        parser.add_argument('-t', '--target', required=True, type=str, help="deployment target")
         return parser
 
     def run(self, id, target, **kwargs):
