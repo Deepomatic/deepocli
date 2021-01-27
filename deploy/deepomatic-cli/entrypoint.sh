fddf28762b9b5fc4f98bd71b42ffc0730ecee681
Thomas Riccardi <thomas@deepomatic.com>
2021-01-27 18:43:02 +0100

    Add a HOME in /tmp/home for DMAKE_UID!=0

--- deploy/deepomatic-cli/entrypoint.sh
+++ deploy/deepomatic-cli/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+# Useful to have a working home (notably for pip commands), otherwise HOME is `/`, which is not writable with DMAKE_UID!=0
+export HOME=/tmp/home
+mkdir -p $HOME
+
 pip install -e .
 
 cat <<"EOF" >> ~/.bashrc
