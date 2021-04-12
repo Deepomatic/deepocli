6d04d11e4b14608c89c6c7f8bde9fcc044972940
alemoreau <zzozolz@gmail.com>
2021-04-12 18:16:23 +0200

    reduce comment length and fix missing variable

--- deepomatic/cli/frame.py
+++ deepomatic/cli/frame.py
@@ -2,13 +2,13 @@ from .thread_base import CurrentMessages
 
 
 class Frame(object):
-    def __init__(self, name, filename, image, video_frame_index=None, absolute_video_frame_index=None):
+    def __init__(self, name, filename, image, decoded_video_frame_index=None, absolute_video_frame_index=None):
         # The Frame object is used as a data exchanged in the different queues
         self.name = name  # name of the frame
         self.filename = filename  # the original filename from which the frame was extracted
         self.image = image  # an opencv loaded image (numpy array)
-        self.decoded_video_frame_index = decoded_video_frame_index  # index of the frame within the video decoded frames (skipped frames are not included)
-        self.absolute_video_frame_index = absolute_video_frame_index  # absolute index of the frame in the video sequence (skipped frames included)
+        self.decoded_video_frame_index = decoded_video_frame_index  # frame index in the input sequence (skipped frames are not included)
+        self.absolute_video_frame_index = absolute_video_frame_index  # frame index in the input sequence (skipped frames included)
         self.frame_number = None  # frame_number since deepocli started (set by input_loop)
         self.inference_async_result = None  # an inference request object that will allow us to retrieve the predictions when ready
         self.predictions = None  # predictions result dict
