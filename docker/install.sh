0ae8592fc75b8be664cee0626e3527a8c278997d
Hugo Maingonnat <hugo@deepomatic.com>
2021-11-03 19:48:06 +0100

    comments

--- docker/install.sh
+++ docker/install.sh
@@ -9,6 +9,8 @@ cd opencv-python
 export CMAKE_ARGS="-DWITH_FFMPEG=ON"
 export ENABLE_CONTRIB=1
 export ENABLE_HEADLESS=1
+# Will use all CPU cores for the compilation
+# You can hardcode a smaller number '-j2' for example
 export MAKEFLAGS="-j$(nproc)"
 
 pip3 wheel . --verbose
