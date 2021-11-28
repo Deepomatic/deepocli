5f3ee90fe7641fe07ae82caa363b55ac6ac2c1c4
Hugo Maingonnat <hugo@deepomatic.com>
2021-11-28 19:40:41 +0100

    Test using ssim

--- tests/test_piping.py
+++ tests/test_piping.py
@@ -1,10 +1,16 @@
 import os
+import time
 import pytest
+import numpy
 import subprocess
 import tempfile
 import hashlib
 import uuid
+import cv2
+import logging
+from skimage.metrics import structural_similarity
 from utils import init_files_setup, DEFAULT_RECOGNITION
+
 
 INPUTS = init_files_setup()
 
@@ -16,29 +22,76 @@ DEEPOCLI_TO_FFMPEG_PIXEL_FORMAT = {
 }
 
 
+def video_ssim(filename_1, filename_2):
+    reader_1 = cv2.VideoCapture(filename_1)
+    assert reader_1.isOpened()
+    reader_2 = cv2.VideoCapture(filename_2)
+    assert reader_2.isOpened()
+    ssim = 0
+    nb_frame = 0
+    while True:
+        read_1, image_1 = reader_1.read()
+        read_2, image_2 = reader_2.read()
+        assert read_1 == read_2
+        if not read_1:
+            break
+        nb_frame += 1
+        ssim += structural_similarity(image_1, image_2, multichannel=True)
+    res = float(ssim) / float(nb_frame)
+    reader_1.release()
+    reader_2.release()
+    return res
+
+
+def check_ssim(filename_1, filename_2, min_value):
+    assert video_ssim(filename_1, filename_2) >= min_value
+
+
+def check_color_diff(color_channel_1, color_channel_2, max_diff):
+    diff = color_channel_1.astype(float) - color_channel_2.astype(float)
+    assert (numpy.absolute(diff) <= max_diff).all()
+
+
+def check_ssim_gray(filename_1, filename_2, min_value):
+    check_ssim(filename_1, filename_2, min_value)
+    reader_2 = cv2.VideoCapture(filename_2)
+    assert reader_2.isOpened()
+    while True:
+        read_2, image_2 = reader_2.read()
+        if not read_2:
+            break
+
+        b, g, r = cv2.split(image_2)
+        # Because of encoding, all channels might not be totally equal
+        check_color_diff(b, g, 4)
+        check_color_diff(b, r, 4)
+
+    reader_2.release()
+
+
 @pytest.mark.parametrize(
-    'infer_cmd,color_space,expected_hash', [
-        ('infer', 'BGR', 'cafba1802a615d7c00122444937c7ea81e084db0cabc5bce18143dffe3534822'),
-        ('draw', 'BGR', '0a8da0d3989d2a8c00082054bbf449556dff10f6432086740604244fb34799a0'),
-        ('blur', 'BGR', '87e3690904d75a783f9bd796a0a1a457f259580f275150bff25ce7404421aa69'),
-        ('noop', 'BGR', 'cafba1802a615d7c00122444937c7ea81e084db0cabc5bce18143dffe3534822'),
+    'infer_cmd,color_space,check_fn,check_args', [
+        ('infer', 'BGR', check_ssim, [0.85]),
+        ('draw', 'BGR', check_ssim, [0.6]),
+        ('blur', 'BGR', check_ssim, [0.59]),
+        ('noop', 'BGR', check_ssim, [0.85]),
 
-        ('infer', 'RGB', 'bd852a31b145265a412fa90a19b367c3b5ad730fa41e85b13959792641f30207'),
-        ('draw', 'RGB', '926ee1e5af5e44c2cf08c874d0556c4f1abfe7b3b63f831283b8313a985a31c1'),
-        ('blur', 'RGB', '9ea0d6db7fd29e79994ebb0998eca476036185a29f7c877b52a4c0df8f54f5ce'),
-        ('noop', 'RGB', 'bd852a31b145265a412fa90a19b367c3b5ad730fa41e85b13959792641f30207'),
+        ('infer', 'RGB', check_ssim, [0.85]),
+        ('draw', 'RGB', check_ssim, [0.6]),
+        ('blur', 'RGB', check_ssim, [0.59]),
+        ('noop', 'RGB', check_ssim, [0.85]),
 
-        ('infer', 'GRAY', '869d300900a902cb5fa622528c551cf3a9305fddc2a64073da8f99d49a8f8cf0'),
-        ('draw', 'GRAY', 'b3a9302692fcb40921bd362e6c0592d6dda9f206f8ec1a968cb529e278e9e75f'),
-        ('blur', 'GRAY', 'ac6f2057602e1e92bd59c0084740904d858508f49205584731f71d8b85f77ddc'),
-        ('noop', 'GRAY', '869d300900a902cb5fa622528c551cf3a9305fddc2a64073da8f99d49a8f8cf0'),
+        ('infer', 'GRAY', check_ssim_gray, [0.81]),
+        ('draw', 'GRAY', check_ssim_gray, [0.6]),
+        ('blur', 'GRAY', check_ssim_gray, [0.55]),
+        ('noop', 'GRAY', check_ssim_gray, [0.81]),
 
-        ('infer', 'YUV420', 'cc41aa34e0b4c7e24c8e261c301a5fb1130b7e4944698eda4d05c22df3900c3f'),
-        ('draw', 'YUV420', 'dbd17585bc48be8e44bfa370635725837913353956d58d7ad3ba746b5843b836'),
-        ('blur', 'YUV420', 'e63448ebec9e78743c2cd1aa728d445db278a7ced8ff38ff057cc9870031fabd'),
-        ('noop', 'YUV420', 'cc41aa34e0b4c7e24c8e261c301a5fb1130b7e4944698eda4d05c22df3900c3f'),
+        ('infer', 'YUV420', check_ssim, [0.85]),
+        ('draw', 'YUV420', check_ssim, [0.6]),
+        ('blur', 'YUV420', check_ssim, [0.59]),
+        ('noop', 'YUV420', check_ssim, [0.85]),
     ])
-def test_piping_ffmpeg(infer_cmd, color_space, expected_hash):
+def test_piping_ffmpeg(infer_cmd, color_space, check_fn, check_args):
     if infer_cmd != 'noop':
         cli_args = '-r ' + DEFAULT_RECOGNITION
     else:
@@ -57,9 +110,7 @@ def test_piping_ffmpeg(infer_cmd, color_space, expected_hash):
     try:
         cmd = cmd_base.format(infer_cmd, video_filename, cli_args, color_space, ffmpeg_pixel_format, codec, output_video)
         subprocess.run(cmd, shell=1, check=1)
-        # Maybe compare ssim / psnr instead of hash if default params of ffmpeg changes
-        with open(output_video, 'rb') as f:
-            assert hashlib.sha256(f.read()).hexdigest() == expected_hash
+        check_fn(video_filename, output_video, *check_args)
     finally:
         if os.path.exists(output_video):
-            os.remove(output_video)
+           os.remove(output_video)
