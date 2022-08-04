557843ea1e6f1d4c099c00afec1143dc85e603f7
Alexandre Moreau <alexandre@deepomatic.com>
2022-08-04 15:42:53 +0200

    add tests for STUDIO_JSON

--- tests/test_blur.py
+++ tests/test_blur.py
@@ -105,6 +105,24 @@ def test_e2e_directory_blur(outputs, expected, no_error_logs):
     run_blur(INPUTS['DIRECTORY'], outputs, **expected)
 
 
+
+
+# # ------- Studio Input Tests -------------------------------------------------------------------------------------- #
+
+
+@pytest.mark.parametrize(
+    'outputs,expected',
+    [
+        ([OUTPUTS['IMAGE']], {'expect_nb_image': 1}),
+        ([OUTPUTS['VIDEO']], {'expect_nb_video': 1}),
+        ([OUTPUTS['STD']], {}),
+        ([OUTPUTS['DIR']], {'expect_nb_subdir': 1, 'expect_subir': {OUTPUTS['DIR']: {'expect_nb_image': 2, 'expect_nb_subdir': 1}}}),
+    ]
+)
+def test_e2e_studio_blur(outputs, expected, no_error_logs):
+    run_blur(INPUTS['STUDIO_JSON'], outputs, **expected)
+
+
 # # ------- Special Options Tests -------------------------------------------------------------------------------------- #
 
 
